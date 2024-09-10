2455f5f96f93fdb4793fc0ec29e3461b760e9729
Jacob <jacob@notactivelylooking.com>
2024-09-10 17:38:03 +0100

    Updated test and printing.

--- lib/examples/vector.c
+++ lib/examples/vector.c
@@ -1,8 +1,17 @@
+#include <stdio.h>
+
 #include "vector.h"
+
+#define MAX_VALUES 10
 
 int main(void)
 {
-    vector_t *v = newVector(5);
+    vector_t *v = newVector(2);
+    printVector(v);
+    for (int i = 0; i < MAX_VALUES; i++) {
+        vector_append(v, i);
+        printVector(v);
+    }
     freeVector(v);
 
     return 0;
