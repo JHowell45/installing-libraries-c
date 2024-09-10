b33626da2554812febcbcc103bb6fc5bb50945d9
Jacob <jacob@notactivelylooking.com>
2024-09-10 15:55:09 +0100

    Updated typing.

--- lib/include/vector.h
+++ lib/include/vector.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 typedef struct {
-    void **items;
+    int **items;
     size_t capacity;
     size_t size;
 } vector_t;
@@ -12,9 +12,9 @@ typedef struct {
 vector_t *newVector(size_t capacity);
 void freeVector(vector_t *vec);
 
-void vector_append(vector_t *vec, void *item);
+void vector_append(vector_t *vec, int item);
 void vector_pop(vector_t *vec);
-void vector_insert(vector_t *vec, void *item, size_t index);
+void vector_insert(vector_t *vec, int item, size_t index);
 void vector_remove(vector_t *vec, size_t index);
 
 void *vector_get(vector_t *vec, size_t index);
--- lib/src/vector.c
+++ lib/src/vector.c
@@ -16,18 +16,18 @@ void freeVector(vector_t *vec)
     free(vec);
 }
 
-void vector_append(vector_t *vec, void *item)
+void vector_append(vector_t *vec, int item)
 {
     if (vec->items == NULL) {
         vec->items = calloc(vec->capacity, sizeof(item));
     }
-    vec->items[vec->size] = item;
+    vec->items[vec->size] = &item;
     vec->size++;
 }
 void vector_pop(vector_t *vec)
 {
 }
-void vector_insert(vector_t *vec, void *item, size_t index)
+void vector_insert(vector_t *vec, int item, size_t index)
 {
 }
 void vector_remove(vector_t *vec, size_t index)
