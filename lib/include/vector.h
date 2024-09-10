c4504c669de979d30e4374a9d04bf321ab022591
Jacob <jacob@notactivelylooking.com>
2024-09-10 15:22:18 +0100

    basic vector impl.

--- lib/include/vector.h
+++ lib/include/vector.h
@@ -9,4 +9,12 @@ typedef struct {
     size_t size;
 } vector_t;
 
+vector_t *newVector(size_t capacity);
+void freeVector(vector_t *vec);
+
+void vector_append(vector_t *vec, void *item);
+void vector_pop(vector_t *vec);
+void vector_insert(vector_t *vec, void *item, size_t index);
+void vector_remove(vector_t *vec, size_t index);
+
 #endif
