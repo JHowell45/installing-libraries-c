8e1a2310542c4b749ad41176ce227067aced8652
Jacob <jacob@notactivelylooking.com>
2024-09-10 17:38:45 +0100

    should be an array of ints, not an array of pointers to an int.

--- lib/include/vector.h
+++ lib/include/vector.h
@@ -4,19 +4,20 @@
 #include <stdlib.h>
 
 typedef struct {
-    int **items;
+    int *items;
     size_t capacity;
     size_t size;
 } vector_t;
 
 vector_t *newVector(size_t capacity);
 void freeVector(vector_t *vec);
+void printVector(vector_t *vec);
 
 void vector_append(vector_t *vec, int item);
 void vector_pop(vector_t *vec);
 void vector_insert(vector_t *vec, int item, size_t index);
 void vector_remove(vector_t *vec, size_t index);
 
-void *vector_get(vector_t *vec, size_t index);
+int vector_get(vector_t *vec, size_t index);
 
 #endif
